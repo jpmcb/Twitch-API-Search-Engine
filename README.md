890ec2c02026af5a217428ad0a62ffbbbd241446
John McBride <jpmmcbride@gmail.com>
2017-02-08 09:28:13 -0700

    Added Read Me & update files
    1. Update to read me file
    2. Comments in JS file
    3. Spacing and comments in index html
    4. General file readability & spacing update

---
+++ README.md
@@ -0,0 +1,4 @@
+# Twitch API Search Tool
+__________
+
+This simple tool can be used to search games being streamed on Twitch, users streaming on twitch, and find the top games being streamed currently. Link for documentation on the [Twitch API](https://github.com/justintv/Twitch-API). A working version of the app can be [found here](https://jpmcb.github.io/Twitch-API-Search-Engine/). Message me with questions!
--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 //all code by jpmmcbride, find me on twitter if you have questions!
 
-var myGameJson;
-var myUserJson;
-var myStreamJson = [];
+var myGameJson, myUserJson, myStreamJson;
 var baseUrl = 'https://api.twitch.tv/kraken';
 var topGames = '/games/top';
-var popularUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 // When the document is ready, load in the top 10 games streaming on twitch
 $(document).ready(function(){
@@ -13,41 +10,45 @@ $(document).ready(function(){
    type: 'GET',
    url: baseUrl + topGames,
    headers: { 'Client-ID': 'gavj8dbz3tejt4nb5kytimtic0g7v6' },
-   error: function() {alert('there was an error')} ,//change 
+   error: function() {alert('there was an error')}, // change to DOM event 
    success: function(data) {
      $('.col-sm-8').hide();
      myGameJson = data;
-     for (var i = 0; i < myGameJson.top.length; i++) {
-       $("<a href='https://www.twitch.tv/directory/game/" + encodeURIComponent(myGameJson.top[i].game.name) + "' class='list-group-item' target='_blank'>" + myGameJson.top[i].game.name + "</a>").hide().appendTo('#topGamesList').fadeIn(3000); }
+     for (var i = 0; i < myGameJson.top.length; i++) { // add list to DOM
+       $("<a href='https://www.twitch.tv/directory/game/" 
+        + encodeURIComponent(myGameJson.top[i].game.name) 
+        + "' class='list-group-item' target='_blank'>" 
+        + myGameJson.top[i].game.name + "</a>").hide().appendTo('#topGamesList').fadeIn(3000); }
    }
   }) 
 })
 
 
-//when user hits enter, run function to search for games
+//when user hits enter on search bar, run function to search for games
 $(document).keypress(function(e){
   if (e.which == 13) {
     $('.searchedGames').fadeOut(1250);
     $('#descriptor').fadeOut(350).html("hover to see who's streaming").fadeIn(500);
     var channel = document.getElementById('searchBar').value;
     
-    //search for user information
-    
+    // Ajax search for twitch streamerinformation
     $.ajax({
       type: 'GET',
       url: baseUrl + '/search/channels?q=' + channel,
       headers: { 'Client-ID': 'gavj8dbz3tejt4nb5kytimtic0g7v6' },
-      error: function() {alert('there was an error')}, //change this
+      error: function() {alert('there was an error')},  //change to DOM event
       success: function(data) {
         myUserJson = data;
         
         for (var x = 0; x < myUserJson.channels.length; x++) {
-          $("<div id='" + myUserJson.channels[x].name + "'class='searchedGames'><h2><a href='" + myUserJson.channels[x].url + "'target='_blank'>" + myUserJson.channels[x].name + "</a></h2></div>").hide().appendTo('#userContent').fadeIn(1500); 
+          $("<div id='" + myUserJson.channels[x].name + "'class='searchedGames'><h2><a href='" 
+            + myUserJson.channels[x].url + "'target='_blank'>" 
+            + myUserJson.channels[x].name 
+            + "</a></h2></div>").hide().appendTo('#userContent').fadeIn(1500); 
         }
         
-        $('div.searchedGames').hover( function(event){
+        $('div.searchedGames').hover( function(event){ // HOVER event to display if user is currently streaming
           var currentID = event.target.id;
-          //console.log(myStreamJson);
           
           $.ajax({
             type: 'GET',
@@ -56,40 +57,15 @@ $(document).keypress(function(e){
             error: function() {alert('there was an error')}, //change this
             success: function(data) {
               myStreamJson = data;
-              if(myStreamJson.stream) {
+              if(myStreamJson.stream) { // handled by CSS with class
                 $('#' + currentID).addClass('online')
               } else {
                 $('#' + currentID).addClass('offline')
               }
             }
           })
-          //console.log(event);
-          
-          //if (myStreamJson.indexOf(currentID) === -1 ) {
-          //$('#' + currentID).addClass('offline');
-          //} else {
-          //$('#' + currentID).addClass('online')
-          //}
         })
       }
     })
-    
-    
   }
 })
-
-//when user clicks box, finds if user is streaming from created array
-
-/*
-$(document.body).on('click','.searchedGames', function(evt){
-  var currentID = this.id;
-  console.log(currentID);
-  console.log(myStreamJson);
-  if (myStreamJson.indexOf(currentID) === -1 ) {
-    $('#' + currentID).addClass('offline');
-    } else {
-    $('#' + currentID).addClass('online')
-    }
-})
-
-*/
